259e9c71ec8d14e7edca84b498cfd905a8c7e007
Nirav <niravrana03@gmail.com>
2019-04-01 12:42:11 -0500

    Added asp.net identity models to project

---
+++ Fitness/Models/Viewmodel/Fitnessviewmodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fitness.Models.Viewmodel
+{
+    public class Fitnessviewmodel
+    {
+    }
+
+    public class Loginviewmodel
+    {
+        
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
